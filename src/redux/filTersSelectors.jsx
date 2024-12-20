5961c45e97ca3032172cf03f662fae3e0d47cdbc
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-12-20 14:25:44 +0530

    fixed includes issue

--- src/redux/filTersSelectors.jsx
+++ src/redux/filTersSelectors.jsx
@@ -14,17 +14,17 @@ export const allJobsSelector = createSelector(
                 .filter((job) => {
                     if (location?.length === 0) return true
                     const locationString = location?.join("")
-                    return locationString.includes(job.location)
+                    return locationString?.includes(job.location)
                 })
                 .filter((job) => {
                     if (industry?.length === 0) return true
                     const industryString = industry?.join("")
-                    return industryString.includes(job.title)
+                    return industryString?.includes(job.title)
                 })
                 .filter((job) => {
                     if (jobType?.length === 0) return true
                     const jobTypeString = jobType?.join("")
-                    return jobTypeString.includes(job.jobType)
+                    return jobTypeString?.includes(job.jobType)
                 })
             return filterdJobs
         }
