14bb678acea6411452c92db78d1a2fafd8e8eb90
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-12-21 20:52:58 +0530

    aded loading bar in saved jobs

--- src/components/SavedJobsTable.jsx
+++ src/components/SavedJobsTable.jsx
@@ -7,12 +7,14 @@ import Endpoints from "@/network/endpoints"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
 import "./styles/index.css"
+import CustomLoader from "./CustomLoader"
 
 const SavedJobsTable = () => {
     const token = Cookies.get("token")
     const navigate = useNavigate()
     const [savedJob, setSavedJobs] = useState([])
     const [loading, setloading] = useState(false)
+    const [jobsLoading,setJobsLoading] = useState(true)
 
     const handelSveForLAter = async (jobId,objId) => {
         try {
@@ -38,7 +40,7 @@ const SavedJobsTable = () => {
     }
 
     useEffect(() => {
-        const f = async () => {
+        const fetchAppliedJobs = async () => {
             try {
                 const res = await axios.get(`${Endpoints.get_saved_jobs}`, {
                     withCredentials: true,
@@ -49,38 +51,43 @@ const SavedJobsTable = () => {
                 setSavedJobs([...res.data.jobs])
             } catch (error) {
                 console.log(error)
+            }finally{
+                setJobsLoading(false)
             }
         }
-        f()
-    }, [])
+        fetchAppliedJobs()
+    }, [token])
 
     return (
         <div>
-            <Table className="applied-jobs-table">
-                <TableCaption>A list of your Saved jobs</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Saved</TableHead>
-                        <TableHead>Job Role</TableHead>
-                        <TableHead>Company</TableHead>
-                        <TableHead>View</TableHead>
-                        <TableHead className="text-right">Action</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody className="applied-job-table-boy">
-                    {
-                        savedJob?.length <= 0 ? <span>You havent any saved job yet.</span> : savedJob.map((appliedJob) => (
-                            <TableRow key={appliedJob._id}>
-                                <TableCell>{EpochToHuman(appliedJob?.createdAt)}</TableCell>
-                                <TableCell>{appliedJob?.jobId?.title}</TableCell>
-                                <TableCell>{appliedJob?.jobId?.company?.name}</TableCell>
-                                <TableCell className="text-blue-900 underline cursor-pointer" onClick={() => navigate(`/description/${appliedJob._id}`)} >View</TableCell>
-                                <TableCell onClick={()=>handelSveForLAter(appliedJob?.jobId?._id,appliedJob._id)} disabled={loading} className="text-right cursor-pointer hover:text-red-600 hover:underline">Delete</TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
+        {
+            jobsLoading ? <CustomLoader />
+            :  <Table className="applied-jobs-table">
+            <TableCaption>A list of your Saved jobs</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Saved</TableHead>
+                    <TableHead>Job Role</TableHead>
+                    <TableHead>Company</TableHead>
+                    <TableHead>View</TableHead>
+                    <TableHead className="text-right">Action</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody className="applied-job-table-boy">
+                {
+                    savedJob?.length <= 0 ? <span>You havent any saved job yet.</span> : savedJob.map((appliedJob) => (
+                        <TableRow key={appliedJob._id}>
+                            <TableCell>{EpochToHuman(appliedJob?.createdAt)}</TableCell>
+                            <TableCell>{appliedJob?.jobId?.title}</TableCell>
+                            <TableCell>{appliedJob?.jobId?.company?.name}</TableCell>
+                            <TableCell className="text-blue-900 underline cursor-pointer" onClick={() => navigate(`/description/${appliedJob._id}`)} >View</TableCell>
+                            <TableCell onClick={()=>handelSveForLAter(appliedJob?.jobId?._id,appliedJob._id)} disabled={loading} className="text-right cursor-pointer hover:text-red-600 hover:underline">Delete</TableCell>
+                        </TableRow>
+                    ))
+                }
+            </TableBody>
+        </Table>
+        }  
         </div>
     )
 }
